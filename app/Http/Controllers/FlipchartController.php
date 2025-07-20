8be1ffae738da1163af13c43b12d20325e5d4a05
aisyah.azman <aisyahazman0231@gmail.com>
2025-07-20 12:36:24 +0800

    add filpchart

--- app/Http/Controllers/FlipchartController.php
+++ app/Http/Controllers/FlipchartController.php
@@ -4,21 +4,97 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\File;
 
 class FlipchartController extends Controller
 {
+    public function flipchart()
+    {
+        $user = auth()->user();
 
-public function flipchart()
+
+        // Load JSON questions
+        $jsonPath = resource_path('json/flipchart_quiz_questions.json');
+        $content = File::get($jsonPath);
+        $allQuestions = collect(json_decode($content, true));
+
+        // Randomly pick 5 questions
+        $selectedQuestions = $allQuestions->shuffle()->take(5)->map(function ($q) {
+            $options = ['A' => 'Betul', 'B' => 'Salah'];
+            // shuffle($options); // Randomize option order
+            return [
+                'id' => $q['id'],
+                'question' => $q['question'],
+                'options' => $options,
+            ];
+        })->values()->toArray();
+
+        return view('checkin.flipchart', [
+            'isRead' => $user->is_read,
+            'questions' => $selectedQuestions,
+            'quizResult' => session('quiz_result'), // pass result to Blade
+        ]);
+    }
+
+    public function welcome()
 {
-    $user = auth()->user(); // Get currently logged-in user
+    return view('checkin.flipchart0');
+}
 
-    return view('checkin.flipchart', [
+
+public function flipchart2()
+{
+    $user = auth()->user();
+
+    $jsonPath = resource_path('json/flipchart_quiz_questions.json');
+    $content = File::get($jsonPath);
+    $allQuestions = collect(json_decode($content, true));
+
+    $selectedQuestions = $allQuestions->shuffle()->take(5)->map(function ($q) {
+        return [
+            'id' => $q['id'],
+            'question' => $q['question'],
+            'options' => ['A' => 'Betul', 'B' => 'Salah'], // Fixed order
+        ];
+    })->values()->toArray();
+
+    return view('checkin.flipchart2', [
         'isRead' => $user->is_read,
+        'questions' => $selectedQuestions,
+        'quizResult' => session('quiz_result'),
     ]);
 }
 
-public function confirmRead()
+
+    public function submitQuiz(Request $request)
+    {
+        // Load correct answers
+        $jsonPath = resource_path('json/flipchart_quiz_questions.json');
+        $content = File::get($jsonPath);
+        $allQuestions = collect(json_decode($content, true));
+        $correctAnswers = $allQuestions->pluck('correct', 'id')->toArray();
+
+        $answers = $request->input('answers');
+        $correctCount = 0;
+
+        foreach ($answers as $id => $userAnswer) {
+            if (isset($correctAnswers[$id]) && $userAnswer === $correctAnswers[$id]) {
+                $correctCount++;
+            }
+        }
+
+        // Flash result and update user if passed
+        if ($correctCount >= 3) {
+            auth()->user()->update(['is_read' => true]);
+            session()->flash('quiz_result', 'pass');
+        } else {
+            session()->flash('quiz_result', 'fail');
+        }
+
+    return redirect()->back();
+    }
+
+    public function confirmRead()
 {
     $user = Auth::user();
     $user->is_read = true;
@@ -26,5 +102,6 @@ public function confirmRead()
 
     return redirect()->back()->with('success', 'Your confirmation has been recorded.');
 }
+
 
 }
